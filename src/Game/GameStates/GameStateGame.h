85566cbf5f5056979c2a76e8374a5319b912f885
Johann Novak <johann.novak25@gmail.com>
2019-01-26 23:38:22 +0100

    Bind of Player2EventListener in GameStateGame

--- src/Game/GameStates/GameStateGame.h
+++ src/Game/GameStates/GameStateGame.h
@@ -3,35 +3,81 @@
 #include "GameState.h"
 
 #include <ShSDK/ShSDK.h>
+#include "Players/IPlayer2EventListener.h"
 
-class GameStateGame : public GameState
+class GameStateGame : public GameState, public IPlayer2EventListener
 {
 
 public:
+	explicit			GameStateGame						(void);
+	virtual				~GameStateGame						(void) SH_ATTRIBUTE_OVERRIDE;
 
-	explicit			GameStateGame			(void);
-	virtual				~GameStateGame			(void) override;
+	void				init								(void) SH_ATTRIBUTE_OVERRIDE;
+	void				release								(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void				init					(void) override;
-	void				release					(void) override;
+	void				entered								(void) SH_ATTRIBUTE_OVERRIDE;
+	void				exiting								(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void				entered					(void) override;
-	void				exiting					(void) override;
+	void				obscuring							(void) SH_ATTRIBUTE_OVERRIDE;
+	void				revealed							(void) SH_ATTRIBUTE_OVERRIDE;
 
-	void				obscuring				(void) override;
-	void				revealed				(void) override;
+	void				update								(float dt) SH_ATTRIBUTE_OVERRIDE;
 
-	void				update					(float dt) override;
+	void				touchBegin							(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
+	void				touchEnd							(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
+	void				touchMove							(const CShVector2 & pos, float ratio) SH_ATTRIBUTE_OVERRIDE;
 
-	void				touchBegin				(const CShVector2 & pos, float ratio) override;
-	void				touchEnd				(const CShVector2 & pos, float ratio) override;
-	void				touchMove				(const CShVector2 & pos, float ratio) override;
+	//
+	// IPlayer2EventListener interface
+	virtual void		OnEventTypeAvailable				(Player2Event *pEvent) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeUnavailable				(Player2Event *pEvent) SH_ATTRIBUTE_OVERRIDE;
+
+	virtual void		OnEventTypeBegin					(Player2Event *pEvent) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeEnd						(Player2Event *pEvent) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeFinished					(Player2Event *pEvent) SH_ATTRIBUTE_OVERRIDE;
+
+	virtual void		OnTypoGaugeUpdated					(float fNormedValue) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnTypoGaugeFilled					(void) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnTypoGaugeEmptied					(void) SH_ATTRIBUTE_OVERRIDE;
+
+	virtual void		OnEventTypeTypeWordCharacterOK		(int iCharacterIndexOK) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeTypeWordCharacterNOK		(int iCharacterIndexNOK) SH_ATTRIBUTE_OVERRIDE;
+
+	virtual void		OnEventTypeRewardUserPireCoin		(int iPeerCoinAmount) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardUserHP				(int iHPBonus) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardGameSlowMotion		(float fSlowMotionFactor, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardTowerAttack		(int iBonusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardTowerAttackSpeed	(int iBonusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardTowerRadius		(int iBonusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeRewardTowerAttackAOE		(int iBonusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+
+	virtual void		OnEventTypeMalusUserHP				(int iHPMalus) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusGameFastForward		(float iFastForwardFactor, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusGameNewEnemy		(int iEnemyCount) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusGameNewWave			(int iWaveCount) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusTowerAttack			(int iMalusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusTowerAttackSpeed	(int iMalusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusTowerRadius			(int iMalusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
+	virtual void		OnEventTypeMalusTowerAttackAOE		(int iMalusValue, float fDurationTime) SH_ATTRIBUTE_OVERRIDE;
 protected:
 private:
 
 public:
 protected:
 private:
-	ShGUIControlPanel *	m_pMainPanel;
+	ShGUIControlPanel *									m_pMainPanel;
+	ShGUIControlText *									m_pWaveCurrent;
+	ShGUIControlText *									m_pWaveTotal;
+	ShGUIControlText *									m_pUserName;
+	ShGUIControlText *									m_pMoneyValue;
+	ShGUIControlText *									m_pHPValue;
+	ShGUIControlText *									m_pMHPTotal;
+	ShGUIControlRadioButton *							m_pPause;
+	ShGUIControlRadioButton *							m_pPlay;
+	ShGUIControlRadioButton *							m_pFastForward;
+	ShGUIControlButton *								m_pMenu;
+	CShArrayMap<EPlayer2EventType, ShGUIControlImage*>	m_amepModeImages;
+
+	ShGUIControlEditBox *								m_pEditBoxHidden;
 
 };
