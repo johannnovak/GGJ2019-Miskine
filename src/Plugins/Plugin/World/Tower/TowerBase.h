f1493a5b78af39088b741cfb22a2cdcde8b040a5
Bastien <bastien@magestik.fr>
2019-01-27 11:09:45 +0100

    Add GetPosition in TowerBase

--- src/Plugins/Plugin/World/Tower/TowerBase.h
+++ src/Plugins/Plugin/World/Tower/TowerBase.h
@@ -41,17 +41,22 @@ public:
 		focus_biggest_lifebase,
 	};
 
-	explicit		TowerBase			(void);
-	virtual			~TowerBase			(void);
+	explicit			TowerBase			(void);
+	virtual				~TowerBase			(void);
 
-	virtual void	Initialize			(const CShIdentifier & levelIdentifier, EnemyManager * pEnemyManager, ETowerType towerType, EFocusType focusType, const CShVector2 & position, int damages, float attackSpeed, float rangeAOE = -1.0f);
-	virtual void	Release				(void);
+	virtual void		Initialize			(const CShIdentifier & levelIdentifier, EnemyManager * pEnemyManager, ETowerType towerType, EFocusType focusType, const CShVector2 & position, int damages, float attackSpeed, float rangeAOE = -1.0f);
+	virtual void		Release				(void);
 
-	virtual void	Update				(float dt);
+	virtual void		Update				(float dt);
 
-	void			LevelUp				(void);
+	void				LevelUp				(void);
 
-	int				GetCurrentLevel		(void);
+	int					GetCurrentLevel		(void);
+
+	const CShVector2 &	GetPosition			(void) const
+	{
+		return m_vPosition;
+	}
 
 protected:
 
